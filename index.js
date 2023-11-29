e51c45357a950fa129b8dc3170bc25967319f19f
Chinmay <92244743+Charuchandra-12@users.noreply.github.com>
2023-11-29 11:24:31 +0530

    code changed

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!!!');
+  res.send('Hello, World!');
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
-// pipeline testing
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,10 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('Hello World API test', async ({ page }) => {
-  console.log('Before navigating to the URL');
+  // console.log('Before navigating to the URL');
   await page.goto('http://localhost:3000');
-  console.log('After navigating to the URL');
-
+  // console.log('After navigating to the URL');
   const text = await page.innerText('body');
   expect(text).toBe('Hello, World!');
 });
