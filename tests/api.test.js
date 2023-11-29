ef06ec1c820e455724aea0da8df30af7d44f267c
Chinmay <92244743+Charuchandra-12@users.noreply.github.com>
2023-11-29 10:56:46 +0530

    code changed

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,7 +1,10 @@
 const { test, expect } = require('@playwright/test');
 
-test('hello world API test', async ({ page }) => {
+test('Hello World API test', async ({ page }) => {
+  console.log('Before navigating to the URL');
   await page.goto('http://localhost:3000');
+  console.log('After navigating to the URL');
+
   const text = await page.innerText('body');
   expect(text).toBe('Hello, World!');
 });
